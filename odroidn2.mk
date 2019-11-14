88bcf248fae527b8f262f1fc360b296aeffa29d4
Chris KIM <codewalker@hardkernel.com>
2019-11-14 16:29:22 +0900

    ODROID-N2: Upate 20191018 BSP.
    Signed-off-by: Chris KIM <codewalker@hardkernel.com>
    Change-Id: Ibdce21e049921ef00fce48e536dce96bfabb5745

--- odroidn2.mk
+++ odroidn2.mk
@@ -40,9 +40,12 @@ $(call inherit-product, device/hardkernel/$(PRODUCT_DIR)/vendor_prop.mk)
 $(call inherit-product, device/hardkernel/common/products/mbox/product_mbox.mk)
 $(call inherit-product, device/hardkernel/$(PRODUCT_DIR)/device.mk)
 $(call inherit-product-if-exists, vendor/google/products/gms.mk)
-
+#########################################################################
+#
+#                                               Media extension
+#
+#########################################################################
 TARGET_WITH_MEDIA_EXT_LEVEL := 4
-
 #########################################################################
 #
 #                     media ext
@@ -60,7 +63,6 @@ ifeq ($(TARGET_WITH_MEDIA_EXT_LEVEL), 3)
     TARGET_WITH_MEDIA_EXT :=true
     TARGET_WITH_SWCODEC_EXT := true
     TARGET_WITH_CODEC_EXT := true
-    TARGET_WITH_PLAYERS_EXT :=true
 else
 ifeq ($(TARGET_WITH_MEDIA_EXT_LEVEL), 4)
     TARGET_WITH_MEDIA_EXT :=true
@@ -153,6 +155,7 @@ endif
 
 BOARD_CACHEIMAGE_PARTITION_SIZE := 1073741824
 BOARD_CACHEIMAGE_FILE_SYSTEM_TYPE := ext4
+BOARD_RECOVERYIMAGE_PARTITION_SIZE := 25165824
 endif
 
 ########################################################################
@@ -187,6 +190,7 @@ BOARD_WIDEVINE_OEMCRYPTO_LEVEL := 1
 BOARD_PLAYREADY_LEVEL := 1
 TARGET_BUILD_CTS:= true
 TARGET_BUILD_NETFLIX:= true
+TARGET_BUILD_NETFLIX_MGKID := true
 endif
 ########################################################################
 
@@ -310,7 +314,13 @@ $(call inherit-product, device/hardkernel/common/media.mk)
 #
 #########################################################################
 
-$(call inherit-product, build/target/product/languages_full.mk)
+# For all locales, $(call inherit-product, build/target/product/languages_full.mk)
+PRODUCT_LOCALES := en_US en_AU en_IN fr_FR it_IT es_ES et_EE de_DE nl_NL cs_CZ pl_PL ja_JP \
+  zh_TW zh_CN zh_HK ru_RU ko_KR nb_NO es_US da_DK el_GR tr_TR pt_PT pt_BR rm_CH sv_SE bg_BG \
+  ca_ES en_GB fi_FI hi_IN hr_HR hu_HU in_ID iw_IL lt_LT lv_LV ro_RO sk_SK sl_SI sr_RS uk_UA \
+  vi_VN tl_PH ar_EG fa_IR th_TH sw_TZ ms_MY af_ZA zu_ZA am_ET hi_IN en_XA ar_XB fr_CA km_KH \
+  lo_LA ne_NP si_LK mn_MN hy_AM az_AZ ka_GE my_MM mr_IN ml_IN is_IS mk_MK ky_KG eu_ES gl_ES \
+  bn_BD ta_IN kn_IN te_IN uz_UZ ur_PK kk_KZ
 
 #################################################################################
 #
@@ -318,7 +328,7 @@ $(call inherit-product, build/target/product/languages_full.mk)
 #
 #################################################################################
 #ifneq ($(TARGET_BUILD_GOOGLE_ATV), true)
-#BUILD_WITH_PPPOE := true
+#BUILD_WITH_PPPOE := false
 #endif
 
 ifeq ($(BUILD_WITH_PPPOE),true)
@@ -377,6 +387,7 @@ PRODUCT_PACKAGES += \
     update_verifier \
     delta_generator \
     brillo_update_payload \
+    android.hardware.boot@1.0 \
     android.hardware.boot@1.0-impl \
     android.hardware.boot@1.0-service
 endif
