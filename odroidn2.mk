0a9a425fe3a0998b18bd5b2a996b5380e846e00e
Chris KIM <codewalker@hardkernel.com>
2020-03-19 15:46:55 +0900

    ODROID-N2: Add 32-bit Android.
    Signed-off-by: Chris KIM <codewalker@hardkernel.com>
    Change-Id: I87f5362993ef45981dd7ffccaaebd3bd6ffc5d97

--- odroidn2.mk
+++ odroidn2.mk
@@ -22,8 +22,8 @@ PRODUCT_DIR := odroidn2
 # Dynamic enable start/stop zygote_secondary in 64bits
 # and 32bit system, default closed
 #
-ANDROID_BUILD_TYPE := 64
-TARGET_DYNAMIC_ZYGOTE_SECONDARY_ENABLE := true
+#ANDROID_BUILD_TYPE := 64
+#TARGET_DYNAMIC_ZYGOTE_SECONDARY_ENABLE := true
 
 # Inherit from those products. Most specific first.
 ifeq ($(ANDROID_BUILD_TYPE), 64)
