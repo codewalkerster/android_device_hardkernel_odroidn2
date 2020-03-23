odroid-dev/android_device_hardkernel_odroidc4

ODROID: ini: Separate logic and value from boot.ini.
- After this patch, booting sequence and environment values are separated
  as boot.ini and env.ini files.
  The booting sequence is remained to the boot.ini file.
  The environment values are moved to env.ini file and make it fit as
  ini format.

- Before the patch, boot.ini file also have environment values. So we
  should extract the environment values from the original a boot.ini file.
  And the values should be applied to the env.ini file.
  This procedures is performed during the upgrade.

Signed-off-by: Luke Go <sangch.go@gmail.com>
Change-Id: I5caa67ce59f0ddb6617e7f3c39f415bf29c9416e

device.mk
@@ -128,4 +128,6 @@ PRODUCT_COPY_FILES += \
     device/hardkernel/odroidc4/files/hardkernel-720.bmp.gz:vendor/etc/hardkernel-720.bmp.gz \
     device/hardkernel/odroidc4/files/makebootini:vendor/bin/makebootini \
     device/hardkernel/odroidc4/files/default.prop.template:vendor/etc/default.prop.template \
-    device/hardkernel/odroidc4/files/makedefaultprop:vendor/bin/makedefaultprop
+    device/hardkernel/odroidc4/files/makedefaultprop:vendor/bin/makedefaultprop \
+    device/hardkernel/odroidc4/files/env.ini.template:vendor/etc/env.ini.template \
+    device/hardkernel/odroidc4/recovery/update_ini.sh:vendor/bin/update_ini.sh

codewalkerster/android_device_hardkernel_odroidn2

Change-Id: Ia7a589b9809117ba1750ea307f85ce97e3bac533

device.mk
@@ -127,4 +127,6 @@ PRODUCT_COPY_FILES += \
     device/hardkernel/odroidn2/files/hardkernel-720.bmp.gz:vendor/etc/hardkernel-720.bmp.gz \
     device/hardkernel/odroidn2/files/makebootini:vendor/bin/makebootini \
     device/hardkernel/odroidn2/files/default.prop.template:vendor/etc/default.prop.template \
-    device/hardkernel/odroidn2/files/makedefaultprop:vendor/bin/makedefaultprop
+    device/hardkernel/odroidn2/files/makedefaultprop:vendor/bin/makedefaultprop \
+    device/hardkernel/odroidn2/files/env.ini.template:vendor/etc/env.ini.template \
+    device/hardkernel/odroidn2/recovery/update_ini.sh:vendor/bin/update_ini.sh
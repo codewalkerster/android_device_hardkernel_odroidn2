3da9eefb3c6f0858becbd038e50eb34102427bf6
Luke Go <sangch.go@gmail.com>
2020-01-29 14:01:23 +0900

    ODROID: ini: Separate logic and value from boot.ini.
    - After this patch, booting sequence and environment values are separated
      as boot.ini and env.ini files.
      The booting sequence is remained to the boot.ini file.
      The environment values are moved to env.ini file and make it fit as
      ini format.
    
    - Before the patch, boot.ini file also have environment values. So we
      should extract the environment values from the original a boot.ini file.
      And the values should be applied to the env.ini file.
      This procedures is performed during the upgrade.
    
    Change-Id: Ia7a589b9809117ba1750ea307f85ce97e3bac533
    Signed-off-by: Luke Go <sangch.go@gmail.com>

---
+++ recovery/update_ini.sh
@@ -0,0 +1,31 @@
+#!/sbin/sh
+Keys="hdmimode voutmode display_autodetect backlight_pwm zoom_rate colorattribute osd_reverse video_reverse suspend_hdmiphy cvbsmode disablehpd disable_vu7 touch_invert_x touch_invert_y test_mt_vid test_mt_pid max_freq_a73 max_freq_53 governor_a73 governor_a53 enable_wol heartbeat sg_tablesize"
+
+TARGET="/odm/env.ini"
+SEDCOMMAND="/odm/.sedcommand"
+
+#should three argus, $1 - boot.ini, $2 - env.ini, $3 applied ini file.
+if [[ -f "$TARGET" ]]; then
+	# using env.ini, $2
+	while read KEY VALUE ETC
+	do
+		if [[ "$Keys" == *"$KEY"* ]]; then
+			command echo "s/^$KEY=\".*\"/$KEY=$VALUE/" >> $SEDCOMMAND
+		fi
+	done < $2
+else
+	# using boot.ini, $1
+	while read KEY VALUE ETC
+	do
+		if [[ "$SET" == "setenv" ]]; then
+			if [[ "$Keys" == *"$KEY"* ]]; then
+				command echo "s/^$KEY=\".*\"/$KEY=$VALUE/" >> $SEDCOMMAND
+			fi
+		fi
+	done < $2
+fi
+
+# apply patch to .env.ini.update, $3
+sed -i -f $SEDCOMMAND $3
+
+rm $SEDCOMMAND
