c6e818142888b7945c22fe74c799d5ace87aad09
Luke Go <sangch.go@gmail.com>
2020-04-27 18:40:08 +0900

    ODROID-N2: Supply dtb overlay.
    - Support Device tree overlay. To use this feature, we can choose
    peripehral states and options. You can on/off the peripehral likes i2c2,
    i2c and other things.
    
    Change-Id: Id758c444903c2eeb3db451063544bb6b652fbf25

--- Kernel.mk
+++ Kernel.mk
@@ -10,6 +10,11 @@ INSTALLED_KERNEL_TARGET := $(PRODUCT_OUT)/kernel
 BOARD_MKBOOTIMG_ARGS := --kernel_offset $(BOARD_KERNEL_OFFSET)
 
 KERNEL_DEVICETREE := meson64_odroidn2_android
+DTB_OUT := $(PRODUCT_OUT)/obj/KERNEL_OBJ/arch/$(KERNEL_ARCH)/boot/dts/amlogic
+DTBO_OUT := $(DTB_OUT)/overlays/odroidn2
+KERNEL_DTBO += $(DTBO_OUT)/*.dtbo
+INTERMEDIATES_DTBS := $(PRODUCT_OUT)/dtbs
+
 KERNEL_DEFCONFIG := odroidn2_android_defconfig
 KERNEL_ARCH := arm64
 
@@ -80,7 +85,15 @@ savekernelconfig: $(KERNEL_OUT) $(KERNEL_CONFIG)
 build-modules-quick:
 	    $(media-modules)
 
-$(INSTALLED_KERNEL_TARGET): $(INTERMEDIATES_KERNEL) | $(ACP)
+$(INTERMEDIATES_DTBS): $(INTERMEDIATES_KERNEL)
+	mkdir -p $(INTERMEDIATES_DTBS)
+	cp $(DTB_OUT)/$(KERNEL_DEVICETREE).dtb $(KERNEL_DTBO) \
+		$(INTERMEDIATES_DTBS)
+
+$(PRODUCT_OUT)/dtbs.img: $(INTERMEDIATES_DTBS)
+	mkfs.cramfs $^ $@
+
+$(INSTALLED_KERNEL_TARGET): $(INTERMEDIATES_KERNEL) $(PRODUCT_OUT)/dtbs.img | $(ACP)
 	@echo "Kernel installed"
 	$(transform-prebuilt-to-target)
 
