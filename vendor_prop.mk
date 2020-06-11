abae2d5bde97791fe9c5a51e2ce41b879698ad10
Luke Go <sangch.go@gmail.com>
2020-06-11 16:38:35 +0900

    ODROID: dolbyvision: Disable dolbyvision feature.
    Change-Id: Iec2b7b1b59aa15961e18d38f2bb13ced8f5edb7d

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -43,7 +43,7 @@ PRODUCT_PROPERTY_OVERRIDES +=  \
     ro.net.pppoe=true
 
 PRODUCT_PROPERTY_OVERRIDES += \
-   ro.vendor.platform.support.dolbyvision=true
+   ro.vendor.platform.support.dolbyvision=false
 
 #the prop is used for enable or disable
 #DD+/DD force output when HDMI EDID is not supported
@@ -71,7 +71,7 @@ PRODUCT_PROPERTY_OVERRIDES +=  \
 
 
 PRODUCT_PROPERTY_OVERRIDES += \
-    media.support.dolbyvision = true
+    media.support.dolbyvision = false
 
 #add for video boot, 1 means use video boot, others not .
 PRODUCT_PROPERTY_OVERRIDES +=  \
