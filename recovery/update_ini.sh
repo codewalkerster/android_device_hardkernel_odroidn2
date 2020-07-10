fe7f6755e48aa701dafebcf5b18ca4e26f2eaa1e
Luke Go <sangch.go@gmail.com>
2020-07-10 10:54:42 +0900

    ODROID-N2: update: ini: Add keys.
    - gpuScaleMode, overlays, overlay_resize options are added to
      update_ini.sh
    
    Change-Id: Ie86b305650ea151a91d4853448b6ee16b71ce8aa

--- recovery/update_ini.sh
+++ recovery/update_ini.sh
@@ -1,5 +1,5 @@
 #!/sbin/sh
-Keys="hdmimode voutmode display_autodetect backlight_pwm zoom_rate colorattribute osd_reverse video_reverse suspend_hdmiphy cvbsmode disablehpd disable_vu7 touch_invert_x touch_invert_y test_mt_vid test_mt_pid max_freq_a73 max_freq_53 governor_a73 governor_a53 enable_wol heartbeat sg_tablesize adjustScreenWay screenAlignment autoFramerate"
+Keys="hdmimode voutmode display_autodetect backlight_pwm zoom_rate colorattribute osd_reverse video_reverse suspend_hdmiphy cvbsmode disablehpd disable_vu7 touch_invert_x touch_invert_y test_mt_vid test_mt_pid max_freq_a73 max_freq_53 governor_a73 governor_a53 enable_wol heartbeat sg_tablesize adjustScreenWay screenAlignment autoFramerate gpuScaleMode overlays overlay_resize"
 
 TARGET="/odm/env.ini"
 SEDCOMMAND="/odm/.sedcommand"
